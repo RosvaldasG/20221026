7d62c768038c138b9d8c523775413a7d46f6414c
RosvaldasG <rosvaldasggg@gmail.com>
2022-11-14 18:45:37 +0200

    with status updated

---
+++ to-do-app-main/api/controllers/tasks.js
@@ -0,0 +1,68 @@
+const TaskSchema = require("../models/taskModel");
+
+// const tasks = [];
+
+module.exports.GET_TASKS = function (req, res) {
+  TaskSchema.find().then((results) => {
+    return res.status(200).json({ tasks: results });
+  });
+};
+
+module.exports.GET_TASK = function (req, res) {
+  TaskSchema.findOne({ _id: req.params.id }).then((results) => {
+    return res.status(200).json({ tasks: results });
+  });
+};
+
+module.exports.INSERT_TASK = function (req, res) {
+  const task = new TaskSchema({
+    task: req.body.task,
+    isDone: req.body.isDone,
+  });
+
+  task.save().then((result) => {
+    return res.status(200).json({
+      statusMessage: "task was inserted successfully",
+      result: result,
+    });
+  });
+};
+
+module.exports.EDIT_TASK = (req, res) => {
+  TaskSchema.updateOne(
+    { _id: req.params.id },
+    { task: req.body.editedTask }
+  ).then((result) => {
+    res
+      .status(200)
+      .json({ statusMessage: "Eddited successfully", editedTask: result });
+  });
+};
+
+module.exports.CHANGE_TASK_STATUS = async (req, res) => {
+  const currentTaskData = await TaskSchema.findOne({
+    _id: req.params.id,
+  }).exec();
+
+  // console.log("currentTask", currentTaskData.isDone);
+
+  TaskSchema.updateOne(
+    { _id: req.params.id },
+    { isDone: !currentTaskData.isDone }
+  ).then((result) => {
+    res
+      .status(200)
+      .json({ statusMessage: "Eddited successfully", editedTask: result });
+  });
+};
+
+module.exports.DELETE_TASK = (req, res) => {
+  TaskSchema.deleteMany({ isDone: req.params.id }).then((results) => {
+    console.log("results", results);
+    res.status(200).json({
+      statusMessage: "Item was deleted sucessfuly",
+
+      deletedItem: results,
+    });
+  });
+};
