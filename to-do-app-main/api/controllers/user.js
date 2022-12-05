f3423242fd75907562e4cbc8c2ab0496797234d7
RosvaldasG <rosvaldasggg@gmail.com>
2022-12-05 12:12:32 +0200

    LOGIN added

--- to-do-app-main/api/controllers/user.js
+++ to-do-app-main/api/controllers/user.js
@@ -1,18 +1,31 @@
+const bcrypt = require("bcryptjs");
+const { rawListeners } = require("../models/userModel");
 const UserSchema = require("../models/userModel");
 const ObjectId = require("mongoose").Types.ObjectId;
 
-module.exports.CREATE_USER = function (req, res) {
+module.exports.CREATE_USER = async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
   const user = new UserSchema({
     name: req.body.name,
     email: req.body.email,
+    password: hashedPassword,
     taskIds: [],
     totalTasksDone: 0,
   });
 
-  user.save().then((result) => {
-    console.log(result);
-    return res.status(200).json({ response: "User was created succses" });
-  });
+  user
+    .save()
+    .then((result) => {
+      console.log(result);
+      return res
+        .status(200)
+        .json({ response: "User was created succses", user });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.status(500).json({ responce: "Failed" });
+    });
 };
 
 module.exports.GET_USERS = function (req, res) {
@@ -37,4 +50,19 @@ module.exports.GET_USER = async function (req, res) {
   console.log(data);
 
   return res.status(200).json({ user: data });
+};
+
+module.exports.USER_LOGIN = async (req, res) => {
+  const user = await UserSchema.findOne({ email: req.body.email });
+
+  const isPasswordMatch = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+
+  if (isPasswordMatch) {
+    return res.status(200).json({ status: "LOGIN successfull" });
+  }
+
+  return res.status(401).json({ status: "LOGIN failed" });
 };
--- to-do-app-main/api/routes/user.js
+++ to-do-app-main/api/routes/user.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { CREATE_USER, GET_USERS, GET_USER } = require("../controllers/user");
+const {
+  CREATE_USER,
+  GET_USERS,
+  GET_USER,
+  USER_LOGIN,
+} = require("../controllers/user");
 
 router.post("/createUser", CREATE_USER);
 router.get("/getUser", GET_USERS);
 router.get("/getUser/:id", GET_USER);
+router.get("/login", USER_LOGIN);
 
 module.exports = router;
